a6610a38ff0e48896516ad088d1a532ba5bae8f5
Tanay Chauli <30913633+tanaychaulinsec@users.noreply.github.com>
2021-02-26 20:24:57 +0530

    add comment

--- basic_api_calling.py
+++ basic_api_calling.py
@@ -3,6 +3,6 @@ api_url="https://ifsc.razorpay.com/"
 ifsc_code="SBIN0000300"
 req = requests.get(api_url+ifsc_code)
 data=req.json()
-#t=data["list"]
+#iterate data dic
 for key , val in data.items():
     print(key +" : "+str(val))
